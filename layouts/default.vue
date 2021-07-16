ba59c8cd3cefd6124df2dbcf0e1432adcc42cb11
reshreshus <reshreshus@gmail.com>
2021-07-16 12:04:18 +0300

    please donate to my monero wallet

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-full">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeafultLayout'
+}
+</script>
+
+<style lang="sass" >
+#__layout
+  @apply h-full
+  height: 100%
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <Tutorial />
+  <div class="h-full w-4/5 mx-auto flex flex-col items-center justify-center">
+    <img :src="require('@/assets/ryam-cropped-min.jpg')" alt="Ryam" class="max-w-md w-4/5 mx-auto rounded"/>
+    <div class="mt-8 text-5xl">
+      Hello there, this is Ryam.
+    </div>
+    <div class="text-3xl">
+      I am the most woke empty-house billionaire developer, <br>
+      in particular, a world class S-rank web developer.</div>
+    <div class="mt-4 text-4xl w-full text-center">
+      Donate to my Monero wallet
+      <div class="text-2xl break-words">
+        43rL42d12AZV2TPECztzuF7VBg5y7U279dJQaLoefHcgDxDpsNqhpE4ftGtWdqaSs3WTn11jY8NRHabKDt5BqYfCVGq6UoS
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="sass">
+#__nuxt, #__layout, body, html
+  @apply h-full
+</style>
